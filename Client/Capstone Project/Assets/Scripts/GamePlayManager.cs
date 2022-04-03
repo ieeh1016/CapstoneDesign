c634863fc7094adb207d5168b0676a8d1dfe03e4
YW <ieeh0122@naver.com>
2022-04-03 22:51:20 +0900

    Main and SelectStage
    Main and SelectStage

---
+++ Client/Capstone Project/Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Button Click");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
