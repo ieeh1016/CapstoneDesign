1c4d1ffe15aee79609dedbb4190aa4cbcee49391
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2022-04-17 19:16:22 +0900

    캐릭터 이동하는 애니메이션 + 걷는 효과음

--- Client/Capstone Project/Assets/Resources/BlocksEngine2/Scripts/EnvironmentElements/BE2_TargetObject.cs
+++ Client/Capstone Project/Assets/Resources/BlocksEngine2/Scripts/EnvironmentElements/BE2_TargetObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
@@ -22,7 +23,7 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
 
     }
 
-    CharacterState _state = CharacterState.Idle;
+    CharacterState _state;
 
     int currentPositionInMap;
 
@@ -48,16 +49,37 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
     public Transform Transform => transform;
     public I_BE2_ProgrammingEnv ProgrammingEnv { get; set; }
 
+    Animator anim;
+
+    Queue<Vector3> TargetQueue = new Queue<Vector3>();
+    Vector3 _target;
+
+    void Start()
+    {
+        _target = this.gameObject.transform.position;
+        _state = CharacterState.Idle;
+        anim = GetComponent<Animator>();
+        startAnimation(_state);
+    }
+    
     void Update()
     {
-        switch (_state)
+        moveToTarget();
+    }
+
+    void startAnimation(CharacterState state)
+    {
+        switch (state)
         {
 
             case CharacterState.Idle:
+                //idle
+                anim.SetInteger("animation", 1);
                 break;
 
             case CharacterState.Moving:
-                UpdateMovintAnimation();
+                //run
+                anim.SetInteger("animation", 12);
                 break;
 
             case CharacterState.Turn:
@@ -65,9 +87,38 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
         }
     }
 
+    //캐릭터가 이동하는 모습을 위한 메서드
+    void moveToTarget()
+    {
+        Vector3 dir = _target - this.gameObject.transform.position;
+
+        if (dir.magnitude < 0.001f)
+        {
+            _state = CharacterState.Idle;
+            startAnimation(_state);
+
+            if(TargetQueue.Count > 0)
+                _target = TargetQueue.Dequeue();
+        }
+        else
+        {
+            _state = CharacterState.Moving;
+            startAnimation(_state);
+            this.gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, _target, 0.1f);
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+        }
+
+        
+    }
+
     [SerializeField]
     float _speed = 10.0f;
     float _counter = 1.0f;
+
+
+
+
+
     void I_BE2_TargetObject.Move()
     {
         GameObject newBlock = null;
@@ -78,49 +129,33 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
                 return;
 
 
+            Vector3 targetPostion = newBlock.transform.position + new Vector3(0, 0.9f, 0);
+            TargetQueue.Enqueue(targetPostion);
 
-            this.gameObject.transform.position = newBlock.transform.position + new Vector3(0, 0.9f, 0);
+            Vector3 dir = targetPostion - this.gameObject.transform.position;
+
             currentBlock = newBlock;
         }
 
-
-
-        //if (_counter > 0)
-        //{
-        //    _counter -= Time.deltaTime;
-        //}
-        //else
-        //{
-        //    _counter = 0;
-        //}
-
-        _state = CharacterState.Idle;
     }
 
     void I_BE2_TargetObject.Turn(bool clockWise)
     {
         if (clockWise)
         {
-            //여기에 우회전 애니메이션 
+
 
 
             currentDirection = (currentDirection + 1) % (int)Direction.size;
-            this.gameObject.transform.forward = this.gameObject.transform.right;
         }
         else
         {
-            //여기에 좌회전 애니메이션 
 
             currentDirection = (currentDirection + (int)Direction.size - 1) % (int)Direction.size;
-            this.gameObject.transform.forward = -this.gameObject.transform.right;
         }
     }
 
-    void UpdateMovintAnimation()
-    {
-        Animator anim = GetComponent<Animator>();
-        anim.Play("fly");
-    }
+
 
 
 
@@ -132,16 +167,16 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
         {
             if (newBlock == null)
             {
-                Debug.Log("오른쪽 불가능1");
+                //Debug.Log("오른쪽 불가능1");
                 return false;
 
             }
 
-            Debug.Log("오른쪽 가능");
+            //Debug.Log("오른쪽 가능");
             return true;
         }
 
-        Debug.Log("오른쪽 불가능2");
+        //Debug.Log("오른쪽 불가능2");
         return false;
     }
 
@@ -153,16 +188,16 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
         {
             if (newBlock == null)
             {
-                Debug.Log("왼쪽 불가능1");
+                //Debug.Log("왼쪽 불가능1");
                 return false;
 
             }
 
-            Debug.Log("왼쪽 가능");
+            //Debug.Log("왼쪽 가능");
             return true;
         }
 
-        Debug.Log("왼쪽 불가능2");
+        //Debug.Log("왼쪽 불가능2");
         return false;
     }
 
@@ -174,16 +209,16 @@ public class BE2_TargetObject : MonoBehaviour, I_BE2_TargetObject
         {
             if (newBlock == null)
             {
-                Debug.Log("전진 불가능1");
+                //Debug.Log("전진 불가능1");
                 return false;
 
             }
 
-            Debug.Log("전진 가능");
+            //Debug.Log("전진 가능");
             return true;
         }
 
-        Debug.Log("전진 불가능2");
+        //Debug.Log("전진 불가능2");
         return false;
     }
 
---
+++ Client/Capstone Project/Assets/Resources/Suriyun/Scripts/AnimatorSound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSound : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<AudioSource>().Play();
+    }
+}
