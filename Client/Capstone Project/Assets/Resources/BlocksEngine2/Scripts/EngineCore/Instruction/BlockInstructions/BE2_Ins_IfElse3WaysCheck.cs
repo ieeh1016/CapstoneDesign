812ddd772117721109110ee46234010f5d6323c7
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2022-04-23 17:31:55 +0900

    ifelse 블록들 추가

---
+++ Client/Capstone Project/Assets/Resources/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/BE2_Ins_IfElse3WaysCheck.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BE2_Ins_IfElse3WaysCheck : BE2_InstructionBase, I_BE2_Instruction
+{
+    //protected override void OnAwake()
+    //{
+    //
+    //}
+
+    //protected override void OnStart()
+    //{
+    //    
+    //}
+
+    //protected override void OnUpdate()
+    //{
+    //
+    //}
+
+    I_BE2_BlockSectionHeaderInput _input0;
+    string _value;
+    bool _isFirstPlay = true;
+
+    protected override void OnButtonStop()
+    {
+        _isFirstPlay = true;
+    }
+
+    public override void OnStackActive()
+    {
+        _isFirstPlay = true;
+    }
+
+    public new void Function()
+    {
+        if (_isFirstPlay)
+        {
+            //EndLoop = true;
+
+            if (TargetObject.AbleRight() && TargetObject.AbleLeft() && TargetObject.AbleForward())
+            {
+                _isFirstPlay = false;
+                ExecuteSection(0);
+            }
+            else
+            {
+                _isFirstPlay = false;
+                ExecuteSection(1);
+            }
+        }
+        else
+        {
+            _isFirstPlay = true;
+            ExecuteNextInstruction();
+        }
+    }
+}
