460b926f2d3367c243bdde974c539f8af430f940
ManInJoongTham <75526576+JK831@users.noreply.github.com>
2022-04-25 20:04:00 +0900

    팝업 수정

---
+++ Client/Capstone Project/Assets/Scripts/UI/Popup/UI_StudyClearPopup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_StudyClearPopup : UI_Popup
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+}
--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -12,7 +12,7 @@ class PacketHandler
 	{
 		C_Request_Name_input pkt = packet as C_Request_Name_input;
 		ClientSession clientSession = session as ClientSession;
-		S_Reply_Name_input s_pkt = packet as S_Reply_Name_input;
+		S_Reply_Name_input s_pkt = new S_Reply_Name_input();
 		s_pkt.reply = Server.DB.DbManager.user_Name_Input(pkt.name, pkt.Uid);
 		clientSession.Send(s_pkt.Write());
 	}
@@ -21,7 +21,7 @@ class PacketHandler
 	{
 		C_Request_Load_Star pkt = packet as C_Request_Load_Star;
 		ClientSession clientSession = session as ClientSession;
-		S_Challenge_Load_Star s_pkt = packet as S_Challenge_Load_Star;
+		S_Challenge_Load_Star s_pkt = new S_Challenge_Load_Star();
 		Dictionary<byte, byte> star_Dic = Server.DB.DbManager.Load_star(pkt.UId);
 		foreach (KeyValuePair<byte, byte> items in star_Dic)
 		{
@@ -38,7 +38,7 @@ class PacketHandler
 	{
 		C_Request_Challenge_MyPage pkt = packet as C_Request_Challenge_MyPage;
 		ClientSession clientSession = session as ClientSession;
-		S_Challenge_MyPage s_pkt = packet as S_Challenge_MyPage;
+		S_Challenge_MyPage s_pkt = new S_Challenge_MyPage();
 		List<Server.DB.DbManager.Data_Structure> data_set = Server.DB.DbManager.Challenge_MyPage(pkt.UId);
 
 		s_pkt.name = Convert.ToString(data_set.Select(x => x.name).Distinct());
@@ -51,7 +51,7 @@ class PacketHandler
 	{
 		C_Request_Challenge_Top30Rank pkt = packet as C_Request_Challenge_Top30Rank;
 		ClientSession clientSession = session as ClientSession;
-		S_Challenge_Top30Rank s_pkt = packet as S_Challenge_Top30Rank;
+		S_Challenge_Top30Rank s_pkt = new S_Challenge_Top30Rank();
 		List<S_Challenge_Top30Rank.Rank> top_30 = new List<S_Challenge_Top30Rank.Rank>();
 		s_pkt.ranks = Server.DB.DbManager.Study_ChallengeTop30(top_30);
 		clientSession.Send(s_pkt.Write());
