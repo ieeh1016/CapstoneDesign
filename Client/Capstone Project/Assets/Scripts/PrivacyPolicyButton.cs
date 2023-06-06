be13d316801c795dbd5fe8e35ce7a7406cce4865
ManInJoongTham <75526576+JK831@users.noreply.github.com>
2023-06-06 09:36:46 +0900

    Add a button link to Privacy-policy

---
+++ Client/Capstone Project/Assets/Scripts/PrivacyPolicyButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyPolicyButton : MonoBehaviour
+{
+    public void PrivacyPolicyURL()
+    {
+        Application.OpenURL("https://jk831.github.io/PrivacyPolicy-KyongMarble/");
+    }
+}
